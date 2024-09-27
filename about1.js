aaa83ffd4b2b1ec2d91341d72ed4297d0a1aa14b
Prince95086 <princeguraru9693@gmail.com>
2024-09-28 05:02:55 +0530

    hii
    i m happy

---
+++ about1.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    // Select both classes to scale up automatically
+    document.querySelector('.text-big').style.transform = 'scale(2.5)';
+    document.querySelector('.text-small1').style.transform = 'scale(2.5)';
+
+    document.querySelector('.big1').style.transform = 'scale(2)';
+    document.querySelector('.small1').style.transform = 'scale(2)';
+
+};
